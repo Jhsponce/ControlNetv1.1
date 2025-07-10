0783506bb45f0e821647098c9e8d281694de0255
Scythd <shambler1@email.com>
2025-07-10 21:26:14 +0200

    Add

--- preprocessor.py
+++ preprocessor.py
@@ -9,21 +9,12 @@ import PIL.Image
 import torch
 from controlnet_aux import (
     CannyDetector,
-    ContentShuffleDetector,
-    HEDdetector,
-    LineartAnimeDetector,
     LineartDetector,
-    MidasDetector,
     MLSDdetector,
-    NormalBaeDetector,
-    OpenposeDetector,
-    PidiNetDetector,
 )
 from controlnet_aux.util import HWC3
 
 from cv_utils import resize_image
-from depth_estimator import DepthEstimator
-from image_segmentor import ImageSegmentor
 
 
 class Preprocessor:
@@ -33,40 +24,22 @@ class Preprocessor:
         self.model: Callable = None  # type: ignore
         self.name = ""
 
-    def load(self, name: str) -> None:  # noqa: C901, PLR0912
+    def load(self, name: str) -> None:
         if name == self.name:
             return
-        if name == "HED":
-            self.model = HEDdetector.from_pretrained(self.MODEL_ID)
-        elif name == "Midas":
-            self.model = MidasDetector.from_pretrained(self.MODEL_ID)
-        elif name == "MLSD":
+        if name == "MLSD":
             self.model = MLSDdetector.from_pretrained(self.MODEL_ID)
-        elif name == "Openpose":
-            self.model = OpenposeDetector.from_pretrained(self.MODEL_ID)
-        elif name == "PidiNet":
-            self.model = PidiNetDetector.from_pretrained(self.MODEL_ID)
-        elif name == "NormalBae":
-            self.model = NormalBaeDetector.from_pretrained(self.MODEL_ID)
         elif name == "Lineart":
             self.model = LineartDetector.from_pretrained(self.MODEL_ID)
-        elif name == "LineartAnime":
-            self.model = LineartAnimeDetector.from_pretrained(self.MODEL_ID)
         elif name == "Canny":
             self.model = CannyDetector()
-        elif name == "ContentShuffle":
-            self.model = ContentShuffleDetector()
-        elif name == "DPT":
-            self.model = DepthEstimator()
-        elif name == "UPerNet":
-            self.model = ImageSegmentor()
         else:
-            raise ValueError
+            raise ValueError(f"Unknown preprocessor: {name}")
         torch.cuda.empty_cache()
         gc.collect()
         self.name = name
 
-    def __call__(self, image: PIL.Image.Image, **kwargs) -> PIL.Image.Image:  # noqa: ANN003
+    def __call__(self, image: PIL.Image.Image, **kwargs) -> PIL.Image.Image:
         if self.name == "Canny":
             if "detect_resolution" in kwargs:
                 detect_resolution = kwargs.pop("detect_resolution")
@@ -75,14 +48,6 @@ class Preprocessor:
                 image = resize_image(image, resolution=detect_resolution)
             image = self.model(image, **kwargs)
             return PIL.Image.fromarray(image)
-        if self.name == "Midas":
-            detect_resolution = kwargs.pop("detect_resolution", 512)
-            image_resolution = kwargs.pop("image_resolution", 512)
-            image = np.array(image)
-            image = HWC3(image)
-            image = resize_image(image, resolution=detect_resolution)
-            image = self.model(image, **kwargs)
-            image = HWC3(image)
-            image = resize_image(image, resolution=image_resolution)
-            return PIL.Image.fromarray(image)
-        return self.model(image, **kwargs)
+
+        image = self.model(image, **kwargs)
+        return PIL.Image.fromarray(image) if isinstance(image, np.ndarray) else image
